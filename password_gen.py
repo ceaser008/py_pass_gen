a0b37a0f9d3492fea59f8b4ffc080b3df1f8c9eb
ceaser <radiusrazzat111@gmail.com>
2022-09-03 22:04:36 +0545

    commited

---
+++ password_gen.py
@@ -0,0 +1,20 @@
+from string import ascii_lowercase,ascii_uppercase,digits,punctuation 
+import random,os
+from colorama import Fore
+lower,upper,num  = [ascii_lowercase], [ascii_uppercase],[digits]
+len = int(input("Length of password: "))
+
+
+def gen_password(len,symbols=False,srength=''):
+    for i in len:
+        
+
+
+
+
+
+
+
+
+
+
