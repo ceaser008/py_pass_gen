4b30bad8639758ef60c1a378370670dbd2ca30eb
razzaT <93041325+ceaser008@users.noreply.github.com>
2022-09-04 16:44:24 +0545

    Update password_gen.py

--- password_gen.py
+++ password_gen.py
@@ -1,29 +1,14 @@
-from string import ascii_lowercase,ascii_uppercase,punctuation,digits
-from colorama import Fore
-import string
-string.punctuation
-import random
-contents=list(ascii_lowercase+ascii_uppercase+digits+punctuation)
-password=[]
-
-length = int(input("len: "))
-for i in range(length):
-    password.append(random.choice(contents))
-random.shuffle(password)
-print(''.join(password))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import random,os,string
+def main():
+    lEN=int(input("Length for your password: "))
+    gen_pass(lEN)
+def gen_pass(lEN,strength=''):
+    pwd=[]
+    lisT=list(string.ascii_uppercase+string.ascii_lowercase+string.digits+string.punctuation )
+    for i in range(lEN):
+        pwd.append(random.choice(lisT))
+    random.shuffle(pwd)
+    print(''.join(pwd))
+    
+if __name__=="__main__":
+	main()
