4440c46b2a64b6917f8223aa272c29848e1bb796
ceaser008 <radiusrazzat111@gmail.com>
2022-12-03 08:28:00 +0545

    workd

---
+++ pass.py
@@ -0,0 +1,21 @@
+#take input from user about : password length, password strength,no of numbers/symbols/characters 
+
+import random
+from string import ascii_letters,ascii_lowercase,ascii_uppercase,punctuation,digits
+
+
+length = int(input("How long should be your password?\n"))
+
+
+def password(length,stren='strong'):
+    for i in range(length):
+
+    
+
+mypass = password(length)
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
--- password_gen.py
+++ password_gen.py
@@ -1,11 +1,13 @@
-import random,os,string
+import os, random, string
+
 def main():
     lEN=int(input("Length for your password: "))
     gen_pass(lEN)
-def gen_pass(lEN,strength=''):
+
+def gen_pass(lEN):
     pwd=[]
     lisT=list(string.ascii_uppercase+string.ascii_lowercase+string.digits+string.punctuation )
-    for i in range(lEN):
+    for items in range(lEN):
         pwd.append(random.choice(lisT))
     random.shuffle(pwd)
     print(''.join(pwd))
